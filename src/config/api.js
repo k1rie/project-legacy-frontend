68ce2c4c1636a4bd4a99faf5a5a9f5cfed7823df
Diego Guerrero <diegoguerrero@Diegos-MacBook-Pro.local>
2025-11-29 17:33:02 -0600

    production api

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 // API Configuration
-export const API_BASE_URL = 'http://localhost:3000/api';
+export const API_BASE_URL = 'https://charming-cassie-k1rie-45484dd5.koyeb.app/api';
 
 export const API_ENDPOINTS = {
   grupos: {
